1dbcc5e5c1482fbd710c6a351dbec6d80ec23aa9
tiwatee <kriswendie4eva@gmail.com>
2022-08-08 02:32:22 +0100

    Add project files.

---
+++ ConcertSelection/Class1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcertSelection
+{
+    public abstract class Class1 //abstraction using abstract class
+    {
+        private bool isadele;
+        private bool isedsheeran;
+        private bool isjohnnydrille;
+        
+        public bool IsAdele //encapsulation
+        {
+            get
+            {
+                return isadele;
+            }
+            set
+            {
+                isadele = value;
+            }
+        }
+        public bool IsEdsheeran //encapsulation
+        {
+            get 
+            { 
+                return isedsheeran; 
+            }
+            set
+            {
+                isedsheeran = value;
+            }
+        }
+
+        public bool IsJohnnyDrille //encapsulation
+        {
+            get
+            {
+                return isjohnnydrille;
+            }
+            set
+            {
+                isjohnnydrille = value;
+            }
+
+        }
+
+        public abstract void ProgOrganizer();
+        public string Attend()
+        {
+            if (IsAdele && IsEdsheeran && IsJohnnyDrille)
+            {
+                return"I will be attending";
+            }
+            else
+            {
+                return "I will not be attending";
+            }
+        }
+    }
+
+    public class AliAndSon : Class1 //inheritance. inheriting the properties and methods of class1
+
+    {
+        public override void ProgOrganizer() //polymorphism
+        {
+            Console.WriteLine("The organizers are Ali and Son Corporation");
+        }
+    }
+
+    public class LadyBug : Class1 // inheritance.Inheriting the properties and methods of class1
+    {
+        public override void ProgOrganizer() //polymorphism
+        {
+            Console.WriteLine("The organizers are Lady Bug Corporation");
+        }
+    }
+
+}
---
+++ ConcertSelection/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConcertSelection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AliAndSon aliAndSon = new AliAndSon();
+            aliAndSon.IsJohnnyDrille=true;
+            aliAndSon.IsEdsheeran=false;
+            aliAndSon.IsAdele=false;
+            Console.WriteLine("{0} the Concert by Ali and Son Corporation",aliAndSon.Attend());
+
+            LadyBug ladyBug = new LadyBug();
+            ladyBug.IsJohnnyDrille=true;
+            ladyBug.IsEdsheeran =true;
+            ladyBug.IsAdele = true;
+            Console.WriteLine("{0} the Concert by Lady Bug Corporation ", ladyBug.Attend());
+        }
+    }
+}
